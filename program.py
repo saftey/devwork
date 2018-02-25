94b3cce97c138cd56b8fd3899befb0752a4d43a0
saftey <greg.crosswhite@gmail.com>
2018-02-24 17:21:23 -0800

    test

--- program.py
+++ program.py
@@ -7,13 +7,14 @@ print()
 
 the_number = random.randint(0, 100)
 
+name = input('Player name? ')
 guess = -1
 
 while guess != the_number:
     guess_text = input('Guess a number between 0 and 100:  ')
     guess = int(guess_text)
     if guess < the_number:
-        print('too low')
+        print(f'{name}, Your Guess of {guess} was too low')
     elif guess > the_number:
         print('too high')
     else:
